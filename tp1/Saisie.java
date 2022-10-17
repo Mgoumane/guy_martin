8f4395920ede9c37b0b0328288b190401afdd534
Mgoumane <goumane.mahmoudou@gmail.com>
2022-10-17 15:06:43 +0200

    first commit

---
+++ tp1/Saisie.java
@@ -0,0 +1,17 @@
+package tp1;
+
+import java.util.Scanner;
+
+public class Saisie {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int x ;
+		System.out.println("Tapez un entier");
+		x = sc.nextInt();
+		System.out.println("Vous avez tapé : " +x);
+		
+	}
+
+}
---
+++ tp1/Variable1.java
@@ -0,0 +1,11 @@
+package tp1;
+
+public class Variable1 {
+	public static void main(String[] args) {
+		int x , y ;
+		x = 50;
+		y = 8;
+		System.out.println(x + y);
+		System.out.println();
+     }
+}
